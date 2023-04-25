5e8c52f2888ea8103f18a19b0c2aa3de01c39823
Rupam <rupammondal564@email.com>
2023-04-25 12:48:58 +0530

    Updated

--- src/Array_2D/question.java
+++ src/Array_2D/question.java
@@ -3,23 +3,16 @@ package Array_2D;
 public class question {
 	public static void main(String args[]) {
 		int[][] arr = {{1 , 2 , 3 , 4 , 5} , {3 , 4 , 5 ,6 , 7} , {7 , 6 , 5 , 4 , 3} , {8 , 7 , 6 , 5 , 4}, {1 , 2 , 37 , 8 , 0}};
+		int k = (arr.length - 1)/2;
 		for(int i = 0 ; i < arr.length ; i++) {
-			for(int j = 0 ; j <arr[i].length ; j++) {
-				if(j == (arr.length - 1)/2) {
-					System.out.print(" "+arr[i][j]);
-				}
-			}
+			System.out.print(" "+arr[i][k]);
 		}
 		for(int i = 0 ; i < arr.length ; i++) {
-			for(int j = 0 ; j <arr[i].length ; j++) {
-				if(i == (arr.length - 1)/2) {
-					if(i == j) {
-						continue;
-					}
-					else {
-						System.out.print(" "+arr[i][j]);
-					}
-				}
+			if(i == k) {
+				continue;
+			}
+			else {
+				System.out.print(" "+arr[k][i]);
 			}
 		}
 	}
